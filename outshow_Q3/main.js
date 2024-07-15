d57976dfa9958c5959f67276ff08ead3e7992ea4
Pratik Amrutkar <130049173+PRAtik11-com@users.noreply.github.com>
2024-07-15 20:40:51 +0530

    Add files via upload

---
+++ outshow_Q3/main.js
@@ -0,0 +1,19 @@
+// string
+console.log("Hello, World");
+
+// number
+console.log(42);
+
+// boolean
+console.log(false);
+console.log(true);
+
+// array
+console.log([0, 1, 2, 3, 4, 5]);
+
+// object
+console.log({
+    name: "John",
+    age: 30,
+    city: "New York"
+});
